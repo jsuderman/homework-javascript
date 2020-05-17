0437b2edf498204e68d641daf53bd3eaef4faec9
jsuderman <62914584+jsuderman@users.noreply.github.com>
2020-05-16 23:11:09 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ var generateBtn = document.querySelector("#generate");
 // var lower case letters
 // var special charaters
 // user clicks gererate button
-  //prompt for amount 8- 128 
-    //if > 8 and < 128 - alert that it must be 8-128 and re enter 
-      // store answer
+//prompt for amount 8- 128 
+//if > 8 and < 128 - alert that it must be 8-128 and re enter 
+// store answer
 
-  //baloon - confirm spiecal characters
-    //if yes store, if no store
-  //baloon - confirm lower case
-    //if yes store, if no store
-  //baloon - confirm cap letters
-    //if yes store, if no store
-  //baloon - confirm numbers
-    //if yes store, if no store
-  
+//baloon - confirm spiecal characters
+//if yes store, if no store
+//baloon - confirm lower case
+//if yes store, if no store
+//baloon - confirm cap letters
+//if yes store, if no store
+//baloon - confirm numbers
+//if yes store, if no store
+
 
 
 
@@ -38,43 +38,103 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePasswod(){
+function generatePasswod() {
 
-function picker(x) {
 
-return (math.floor(Math.random() * x));
 
-}
-  
-var numbers = ["0,1,2,3,4,5,6,7,8,9"]
-var cap = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S,", "T", "U", "V", "W", "X", "Y", "Z"]
-var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-var all = numbers + cap + lower + special;
+  var numbers = ["0,1,2,3,4,5,6,7,8,9"]
+  var cap = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S,", "T", "U", "V", "W", "X", "Y", "Z"]
+  var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+  //var all = numbers + cap + lower + special;
 
-var password = ''; {
+  var allOptions = []
+  var charNeeded = []
+  var passFinal = []
+  var password = '';
 
   var lengthPass = prompt("Choose length between 8 - 128 characters");
-    if(lengthPass <8 || lengthPass >128){
+  if (lengthPass < 8 || lengthPass > 128) {
 
-      alert("Password length must be between 8-128 characters");
-      var lengthPass = prompt("Choose length between 8 - 128 characters");
+    alert("Password length must be between 8-128 characters");
+  } else {
+
+    var low = confirm(" do you want lower case letters in password?")
+    console.log(low);
+
+    var upper = confirm(" do you want upper case letters in password?")
+    console.log(upper);
+
+    var sc = confirm(" do you want special charcters in password?")
+    console.log(sc);
+
+    var num = confirm(" do you want numbers in password?")
+    console.log(num);
+
+
+    if (low) {
+      allOptions = allOptions.concat(lower)
+      getRandomLower()
+
     }
 
-  var low = confirm(" do you want lower case letters in password?")
-  console.log(low);
+    if (upper) {
+      allOptions = allOptions.concat(upper)
+      getRandomUpper()
 
-  var upper = confirm(" do you want upper case letters in password?")
-  console.log(upper);
-  
-  var sc = confirm(" do you want special charcters in password?")
-  console.log(sc);
-  
-  
-for (i=0; i<lengthPass; i++) {
+    }
 
-  )
+    if (sc) {
+      allOptions = allOptions.concat(special)
+      getRandomSpecial()
 
-}
+    }
 
-return password;
+    if (num) {
+      allOptions = allOptions.concat(numbers)
+      getRandomNumber()
+
+    }
+
+
+    for (i = 0; i < lengthPass; i++) {
+      var randomCharacter = allOptions[Math.floor(Math.random() * allOptions.length)]
+      finalPass.push(randomCharacter)
+      console.log(randomCharacter)
+
+    }
+
+    for (var f = 0; f < charNeeded.length; f++) {
+      finalPass[f] = charNeeded[f]
+    }
+
+    finalPassString = finalPass.join("")
+
+
+    return finalPassString
+
+    function getRandomLower() {
+      var randomCharacter = lower[Math.floor(Math.random() * lower.length)]
+      charNeeded.push(randomCharacter)
+
+    }
+
+    function getRandomUpper() {
+      var randomCharacter = cap[Math.floor(Math.random() * cap.length)]
+      charNeeded.push(randomCharacter)
+
+    }
+
+    function getRandomLower() {
+      var randomCharacter = Special[Math.floor(Math.random() * special.length)]
+      charNeeded.push(randomCharacter)
+
+    }
+
+    function getRandomLower() {
+      var randomCharacter = numbers[Math.floor(Math.random() * numbers.length)]
+      charNeeded.push(randomCharacter)
+
+    }
+
+    return passFinal
